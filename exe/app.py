ec9c7037bed1c933636db96b69ca34f3b33cae91
dud4h-1AT <m.epifanio@escola.pr.gov.br>
2025-10-17 11:32:22 -0300

    update

---
+++ exe/app.py
@@ -0,0 +1,85 @@
+player = {
+     '1':{
+          'nome':'Nilson',
+          'idade': 24,
+          'ryos': 320.50,
+          'classe': 'Mago',
+          'familia': 'Loki',
+          'habilidades': {
+              'ataque': 10,
+              'defesa': 1,
+              'vida': 10,
+          },
+          'nivel': 100,
+          'xp': 0,
+          'equipamento': [2],
+          'dungeon': 1,
+    },
+     '2': {
+        'nome': 'Duda',
+        'idade': 15,
+        'ryos': 0.0,
+        'classe': 'Guerreiro',
+        'familia': 'PPJUDAS',
+        'habilidades': {
+            'ataque': 23,
+            'defesa': 1,
+            'vida': 1,
+        },
+        'nivel': 25,
+        'xp': 0,
+        'equipamentos': [0]
+
+    },
+     '2': {}
+}
+ 
+ equipamentos = {
+     '1':{
+        'nome': 'Espada de madeira',
+        'classe': 'Guerreiro',
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 1,}
+    },
+
+     '2':{
+        'nome': 'Livro velho',
+        'classe': 'Mago',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 0,
+            'vida': 1,}
+        },
+
+     '3':{
+        'nome': 'Arco de madeira',
+        'classe': 'Arqueiro',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 2,
+            'defesa': 0,
+            'vida': 0,
+        } }}
+
+monstros = {
+        1: {'nome': 'Rato',
+            'habilidades': {
+                'ataque': 1,
+                'defesa': 0,
+                'vida': 1,}
+        }
+},
+        2: {'nome': 'Formiga',
+            'habilidades': {
+                'ataque': 2,
+                'defesa': 2,
+                'vida': 2,}
+        }
+        3: {'nome': 'Formiga',
+            'habilidades': {
+                'ataque': 4,
+                'defesa': 3,
+                'vida': 1,}
+        }
